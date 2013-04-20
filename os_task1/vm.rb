b6be8824cf3be74273542b703038ac544f047dfc
Sergey Ryabkov <serega0807@gmail.com>
2013-04-20 21:24:47 +0600

    еще доработка

--- os_task1/vm.rb
+++ os_task1/vm.rb
@@ -1,4 +1,5 @@
-$zapp, $zam1, $zam2, $vzap1, $vib, $chist, $pusk, $IR, $IP
+$p, $zapp, $zam1, $zam2, $vzap1, $vib, $chist, $op, $ron, $rvv, $IR, $IP, $IA =
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
 $table_kom = {
   '00' => [1, 0, 0, 0],
@@ -18,31 +19,33 @@ $table_kom = {
 # puts table_kom['21'][3]
 
 def dekkom(arg)
-	p = $table_kom[arg][1]
-  zapp = p == 0 ? 1 : 0
-  zam1 = p == 1 ? 1 : 0
-  vzap1 = p == 3 ? 1 : 0
-  zam2 = p != 3 ? 1 : 0
-  chist = (p == 2 or p == 3) ? 1 : 0
-  pusk = arg != 'ff' ? 1 : 0
-  vyb = $table_kom[arg][0]
+#puts "arg = #{arg.to_s(16)}"
+	$p = $table_kom[arg.to_s(16)][1]
+#puts "p = #{$p}"
+  $zapp = $p == 0 ? 1 : 0
+  $zam1 = $p == 1 ? 1 : 0
+  $vzap1 = $p == 3 ? 1 : 0
+  $zam2 = $p != 3 ? 1 : 0
+  $chist = ($p == 2 or $p == 3) ? 1 : 0
+  $pusk = arg != 'ff' ? 1 : 0
+  $vib = $table_kom[arg.to_s(16)][0]
 end
 
 
 def alu(arg0, arg1)
-	op = $com_table[$kop][2]
-  return arg0 if op == 0
-	return arg1 if op == 1
-	return arg0 + arg1 if op == 2
-	return arg1 - arg0 if op == 3
-	return $ron if op == 15
+#puts "kop = #{$kop.to_s(16)}"
+	$op = $table_kom[$kop.to_s(16)][2]
+#puts "op = #{$op}"
+  return arg0 if $op == 0
+	return arg1 if $op == 1
+	return arg0 + arg1 if $op == 2
+	return arg1 - arg0 if $op == 3
+	return $ron if $op == 15
 	return -1
 end
 
 def m(*args)
-#	return args[args[-1]]
-	return args[1] if args[-1] == 1
-	return args[0] if args[-1] == 0
+	return args[args[-1]]
 end
 
 
@@ -59,17 +62,38 @@ while i < prog.size
   kop = prog[i].hex.to_i
   adr = prog[i+1].hex.to_i
   i+=2
-  #puts "#{kop}, #{adr}"
+#puts "#{kop}, #{adr}"
   cmds[j] = [kop, adr]
   j+=1
 end
-$IP = 0
-$IR = 0
+#$IP = 0
+#$IR = 0
 $pusk = 1
-while true
+#while true
   $kop = cmds[$IP][0]
+#puts "kop = #{$kop}"
   $adr = cmds[$IP][1]
   dekkom $kop
+  $IA = tmp1 = $adr + $IR
+  tmp0 = cmds[tmp1][1]
+  tmp2 = $rvv
+  tmp1 = m(tmp0, tmp1, tmp2, $vib)
+  tmp0 = $ron
+  tmp0 = alu(tmp0, tmp1)
   
+  tmp = m(tmp0, 0, $chist)
+  $IR = tmp if $zam2 == 1
   
-end
+  $ron = tmp0 if $zam1 == 1
+  
+  cmds[$IP + $IA][1] = tmp0
+  
+  tmp = m($IP + 1, $IA, $table_kom[$kop.to_s(16)][3])
+  if $pusk == 1
+    $IP = tmp
+  else
+    break
+  end
+  
+#puts "end"
+#end
