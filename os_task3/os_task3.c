a5b7b8edb1742e80a840860aa1352409b22fcd97
Sergey Ryabkov <serega0807@gmail.com>
2013-04-26 20:43:27 +0600

    разобрался с getmemorymap

--- os_task3/os_task3.c
+++ os_task3/os_task3.c
@@ -3,13 +3,36 @@
  
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	
-	SIMPLE_TEXT_OUTPUT_INTERFACE *conout;
-	
 	InitializeLib(ImageHandle, SystemTable);
-	conout = SystemTable->ConOut;
-
-	uefi_call_wrapper(conout->OutputString, 2, conout, (CHAR16 *)L"Hello World\n\r");
-
+	
+	EFI_STATUS status;
+	
+	// parameters for GetMemoryMap()
+	UINTN memoryMapSize, mapKey, descriptorSize;
+	EFI_MEMORY_DESCRIPTOR *memoryMap;
+	UINT32 descriptorVersion;
+	
+	status = uefi_call_wrapper(SystemTable->BootServices->GetMemoryMap, 5, &memoryMapSize, memoryMap, &mapKey, &descriptorSize, &descriptorVersion);
+	if (status != EFI_BUFFER_TOO_SMALL) {
+		Print(L"error in GetMemoryMap\n");
+		return EFI_SUCCESS;
+	}
+	
+	// for AllocatePool()
+	EFI_MEMORY_TYPE poolType = EfiLoaderData;
+	
+	status = uefi_call_wrapper(SystemTable->BootServices->AllocatePool, 3, poolType, memoryMapSize, ((void*)&memoryMap));
+	if (status != EFI_SUCCESS) {
+		Print(L"error in AllocatePool\n");
+		return EFI_SUCCESS;
+	}
+	
+	status = uefi_call_wrapper(SystemTable->BootServices->GetMemoryMap, 5, &memoryMapSize, memoryMap, &mapKey, &descriptorSize, &descriptorVersion);
+	if (status != EFI_SUCCESS) {
+		Print(L"error in GetMemoryMap\n");
+		return EFI_SUCCESS;
+	}
+	
 	return EFI_SUCCESS;
 	
 }
