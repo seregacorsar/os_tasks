802b6225d654437c9253c93db3e574a591be1a3e
Sergey Ryabkov <serega0807@gmail.com>
2013-04-28 19:17:26 +0600

    доделал

--- os_task3/os_task3.c
+++ os_task3/os_task3.c
@@ -15,7 +15,7 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	status = uefi_call_wrapper(SystemTable->BootServices->GetMemoryMap, 5, &memoryMapSize, memoryMap, &mapKey, &descriptorSize, &descriptorVersion);
 	if (status != EFI_BUFFER_TOO_SMALL) {
 		Print(L"error in GetMemoryMap\n");
-		return EFI_SUCCESS;
+		return status;
 	}
 	
 	// for AllocatePool()
@@ -24,14 +24,24 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	status = uefi_call_wrapper(SystemTable->BootServices->AllocatePool, 3, poolType, memoryMapSize, ((void*)&memoryMap));
 	if (status != EFI_SUCCESS) {
 		Print(L"error in AllocatePool\n");
-		return EFI_SUCCESS;
+		return status;
 	}
 	
 	status = uefi_call_wrapper(SystemTable->BootServices->GetMemoryMap, 5, &memoryMapSize, memoryMap, &mapKey, &descriptorSize, &descriptorVersion);
 	if (status != EFI_SUCCESS) {
 		Print(L"error in GetMemoryMap\n");
-		return EFI_SUCCESS;
+		return status;
 	}
+	
+	long long int res = 0;
+	UINTN i = 0;
+	for (i = 0; i < memoryMapSize; i++)
+		if ( ( memoryMap[i].Type == EfiBootServicesCode ) || ( memoryMap[i].Type == EfiBootServicesData ) || ( memoryMap[i].Type == EfiConventionalMemory ) )
+			res += memoryMap[i].NumberOfPages;
+	
+	Print(L"%d", res * 4096);
+	
+	
 	
 	return EFI_SUCCESS;
 	
