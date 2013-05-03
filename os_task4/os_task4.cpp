b944640a2079605a45f3ded34f59951c12acbaaf
Sergey Ryabkov <serega0807@gmail.com>
2013-05-04 00:12:20 +0600

    вроде работает

--- os_task4/os_task4.cpp
+++ os_task4/os_task4.cpp
@@ -2,17 +2,42 @@
 #include <pthread.h>
 
 void * inc( void * arg);
+void * inc_p( void * arg);
 void * dec( void * arg);
+void * dec_p( void * arg);
 
-int common_var = 0;
+volatile int common_var = 0;
+int ptr = 0;
+
+void enter_critical_section() {
+	while( __sync_val_compare_and_swap( &ptr, 0, 1 ) == 1 ) {}
+	return;
+}
+
+void leave_critical_section() {
+	ptr = 0;
+	return;
+}
 
 int main(int argc, const char * argv[]) {
 	
 	pthread_t t1, t2;
-	pthread_create(&t1, NULL, inc, NULL);
-	pthread_create(&t2, NULL, dec, NULL);
+	for (int i=0; i<5; ++i) {
+		pthread_create(&t1, NULL, inc_p, NULL);
+		pthread_create(&t2, NULL, dec_p, NULL);
+		pthread_join(t1, NULL);
+		pthread_join(t2, NULL);
+		printf("common_var with lock is %d\n", common_var);
+	}
 	
-	printf("common_var is %d", common_var);
+	for (int i=0; i<5; ++i) {
+		pthread_t t1, t2;
+		pthread_create(&t1, NULL, inc, NULL);
+		pthread_create(&t2, NULL, dec, NULL);
+		pthread_join(t1, NULL);
+		pthread_join(t2, NULL);	
+		printf("common_var is %d\n", common_var);
+	}
 	
 	return 0;
 }
@@ -23,8 +48,25 @@ void * inc( void * arg ) {
 	return 0;
 }
 
+void * inc_p( void * arg ) {
+	enter_critical_section();
+	for (int i=0; i<1000000; ++i)
+		common_var++;
+	leave_critical_section();
+	return 0;
+}
+
+
 void * dec( void * arg ) {
 	for (int i=0; i<1000000; ++i)
 		common_var--;
+	return 0;
+}
+
+void * dec_p( void * arg ) {
+	enter_critical_section();
+	for (int i=0; i<1000000; ++i)
+		common_var--;
+	leave_critical_section();
 	return 0;
 }
