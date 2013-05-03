b8ba609fe31ee39a3146f3b8c8f5f55409aafccf
Sergey Ryabkov <serega0807@gmail.com>
2013-05-03 21:30:24 +0600

    без синхронизации

--- os_task4/os_task4.cpp
+++ os_task4/os_task4.cpp
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void * writeY( void * arg);
+void * inc( void * arg);
+void * dec( void * arg);
+
+int common_var = 0;
 
 int main(int argc, const char * argv[]) {
-    pthread_t t;
-	pthread_create(&t, NULL, writeY, NULL);
-	while (true)
-		printf("x");
+	
+	pthread_t t1, t2;
+	pthread_create(&t1, NULL, inc, NULL);
+	pthread_create(&t2, NULL, dec, NULL);
+	
+	printf("common_var is %d", common_var);
 	
 	return 0;
 }
 
-void * writeY( void * arg ) {
-	while (true)
-		printf("y");
+void * inc( void * arg ) {
+	for (int i=0; i<1000000; ++i)
+		common_var++;
+	return 0;
+}
+
+void * dec( void * arg ) {
+	for (int i=0; i<1000000; ++i)
+		common_var--;
 	return 0;
 }
